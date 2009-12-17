49c316e45caf7690bc159caa3e0390b9f00f27cd
Pierre Larochelle <pierre.larochelle@gatech.edu>
2009-12-17 14:21:52 -0500

    changed the poets view to order the poems by last name

--- couch/lib/poem_check.js
+++ couch/lib/poem_check.js
@@ -2,5 +2,5 @@ function sane_poem(doc) {
     var author = doc.author;
     
     // is not "", "Anonymous", or contain any quote characters
-    return author.length > 0 && author != "Anonymous" && author.indexOf('"') == -1;
+    return author.length > 0 && author != "Anonymous" && author.indexOf('"') == -1 && author.indexOf("(") == -1;
 }
--- couch/views/poets/map.js
+++ couch/views/poets/map.js
@@ -2,6 +2,9 @@ function(doc) {
     // !code lib/poem_check.js
 
     if (doc.type == 'poem' && sane_poem(doc)) {
-        emit(doc.author, doc.title);
+	var split_name = doc.author.split(' ');
+	var last_name = split_name.pop();
+        emit(last_name + ", " + split_name.join(' '), doc.title); // looks like key: 'last, first', 
+	                                                          //            value: 'work name'
     }
 }
