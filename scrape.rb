921f3fdc4f395d3f3e9a8f5098509a8f45ae0ab9
Pierre Larochelle <pierre.larochelle@gatech.edu>
2009-12-23 00:33:15 -0500

    changed scrape so that it checks if there is already a poem with the same url already in the db before adding a new one

--- scrape.rb
+++ scrape.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'hpricot'
 require 'open-uri'
 require 'json'
-require 'rest_client'
+require 'couchrest'
 require 'logger'
 
 info = JSON.parse(File.read('config.json'))['couch']
@@ -11,15 +11,23 @@ info = JSON.parse(File.read('config.json'))['couch']
 @starting_url = "http://en.wikisource.org/wiki/Category:Poems"
 @base_url = "http://en.wikisource.org"
 @poem_link_regex = /^\/wiki\/[A-Za-z0-1_():]+$/
-@couch = "http://#{info['username']}:#{info['password']}@#{info['host']}:#{info['port']}/#{info['name']}"
+if info['password'] and info['username']
+  @couch = CouchRest.database!("http://#{info['username']}:#{info['password']}@#{info['host']}:#{info['port']}/#{info['name']}")
+else
+  @couch = CouchRest.database!("http://#{info['host']}:#{info['port']}/#{info['name']}")
+end
 @log = Logger.new "/var/poeticc/log/scrape.log", "daily"
 
 def scrape_poem_page(url)
   sleep 0.05 # so we don't overload the source
   doc = Hpricot(open(url))
   
+  # sometimes the title is a link
+  title = (doc/"#header_title_text").inner_html
+  title = (Hpricot(title)/"a").inner_html if title =~ /<a href/
+  
   {
-    :title      => (doc/"#header_title_text").inner_html,
+    :title      => title,
     :author     => (doc/"#header_author_text").inner_html,
     :body_html  => (doc/".poem").first.inner_html,
     :source     => url,
@@ -38,10 +46,11 @@ def scrape_poems_list(url)
   
   (doc/"li a").select { |link| link.attributes['href'].match(@poem_link_regex) }.each do |link|
     poem_url = @base_url + link.attributes['href']
-    poem = scrape_poem_page(poem_url)
-     
-    if poem
-      poems << poem
+    existing_poem = @couch.view('couch/poem_urls', :key => poem_url)
+    
+    if existing_poem['rows'].empty?
+      poem = scrape_poem_page(poem_url)
+      poems << poem if poem
     end
   end
   poems
@@ -49,13 +58,16 @@ end
 
 
 next_url = @starting_url
+total_poems_saved = 0
 while next_url
   poems = scrape_poems_list next_url
   
   poems.each do |poem|
-    poem_json = JSON.generate poem
-    resp = RestClient.post(@couch, poem_json)
-    @log.info "saved poem with response #{resp}"
+    resp = @couch.save_doc(poem)
+    if resp
+      @log.info "saved poem with response #{resp}"
+      total_poems_saved += 1
+    end
   end
   
   next_links = (Hpricot(open(next_url))/("a")).select { |link| link.inner_html == "next 200" }
@@ -65,3 +77,5 @@ while next_url
     next_url = false
   end  
 end
+
+@log.info "saved #{total_poems_saved} poems"
