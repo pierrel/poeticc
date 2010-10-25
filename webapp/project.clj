349ab1cb6f20c4a31b9d6b5176164e9142d0564d
Pierre Larochelle <pierre.larochelle@gatech.edu>
2010-10-25 00:02:51 -0700

    updated packages to latest

--- webapp/project.clj
+++ webapp/project.clj
@@ -3,10 +3,10 @@
   :dependencies
   [[org.clojure/clojure "1.2.0"]
    [org.clojure/clojure-contrib "1.2.0"]
-   [ring/ring-core "0.2.5"]
-   [ring/ring-jetty-adapter "0.2.5"]
-   [compojure "0.4.0"]
-   [hiccup "0.2.6"]
+   [ring/ring-core "0.3.2"]
+   [ring/ring-jetty-adapter "0.3.2"]
+   [compojure "0.5.2"]
+   [hiccup "0.2.7"]
    [com.ashafa/clutch "0.2.3-SNAPSHOT"]
    [org.danlarkin/clojure-json "1.1"]]
   :dev-dependencies
