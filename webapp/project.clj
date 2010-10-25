4b6288cf09e8d0caacea7bd7701e00431bea2aee
Pierre Larochelle <pierre.larochelle@gatech.edu>
2010-10-24 22:34:24 -0700

    added lein project file and moved controller to src dir

---
+++ webapp/project.clj
@@ -0,0 +1,11 @@
+(defproject poeticc "0.0.1"
+  :description "Show me some poems"
+  :dependencies
+  [[org.clojure/clojure "1.2.0"]
+   [org.clojure/clojure-contrib "1.2.0"]
+   [ring/ring-core "0.2.5"]
+   [ring/ring-jetty-adapter "0.2.5"]
+   [compojure "0.4.0"]
+   [hiccup "0.2.6"]]
+  :dev-dependencies
+  [[lein-run "1.0.0-SNAPSHOT"]])
